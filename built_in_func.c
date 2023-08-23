dc8a68ad370bdd6674291111128bbfc9f62574c3
Ghizo2001 <gabi.66ok@gmail.com>
2023-08-22 19:02:35 -0700

    built_in_func.c

---
+++ built_in_func.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+/**
+ * check_build_in_func - command line interpreter
+ * @final_string: input from user
+ * @envp: array of variables
+ * Return: return value
+ */
+int check_build_in_func(char *final_string, char *envp[])
+{
+	char *array_buitin[] = {"cd", "exit", "env"};
+	int num_builtins = sizeof(array_buitin) / sizeof(array_buitin[0]);
+	int f = 0;
+
+	while (f < num_builtins)
+	{
+		if (my_strcmp(array_buitin[f], final_string) == 0)
+		{
+			switch (f + 1)
+			{
+				case 1:
+					chdir(final_string);
+					return (1);
+
+				case 2:
+					printf("BYE ;)");
+					return (2);
+
+				case 3:
+					my_env(envp);
+					return (1);
+
+				default:
+					return (0);
+			}
+		}
+		f++;
+	}
+	return (0);
+}
+
